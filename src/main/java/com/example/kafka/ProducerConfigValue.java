c87a12e1ca81dfd64c95a10e267cf407a505d642
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-05 18:35:57 +0900

    style: final 키워드 적용

--- src/main/java/com/example/kafka/ProducerConfigValue.java
+++ src/main/java/com/example/kafka/ProducerConfigValue.java
@@ -1,9 +1,9 @@
 package com.example.kafka;
 
 public abstract class ProducerConfigValue {
-    public static String BOOTSTRAP_SERVERS = "localhost:19093,localhost:29093,localhost:39093";
-    public static String ACK_0 = "0";
-    public static String ACK_1 = "1";
-    public static String ACK_ALL = "all"; // -1 값과 동일하다.
-    public static String COMPRESSION_TYPE_GZIP = "gzip";
+    public static final String BOOTSTRAP_SERVERS = "localhost:19093,localhost:29093,localhost:39093";
+    public static final String ACK_0 = "0";
+    public static final String ACK_1 = "1";
+    public static final String ACK_ALL = "all"; // -1 값과 동일하다.
+    public static final String COMPRESSION_TYPE_GZIP = "gzip";
 }
