4e2e5f1eeed6883b24923c72825c7dbfb5752d91
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-05 12:10:44 +0900

    feat: KafkaBookProducerKey 추가

---
+++ src/main/java/com/example/kafka/KafkaBookProducerKey.java
@@ -0,0 +1,56 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class KafkaBookProducerKey {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaBookProducerKey.class);
+
+    public static void main(String[] args) {
+        // Properties 오브젝트 생성
+        Properties props = new Properties();
+
+        // 브로커 리스트를 정의한다.
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ProducerConfigValue.BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, ProducerConfigValue.COMPRESSION_TYPE_GZIP);
+
+        // Ack 모드를 1로 설정
+        props.put(ProducerConfig.ACKS_CONFIG, ProducerConfigValue.ACK_1);
+
+        // 메시지의 키와 값에 문자열을 사용할 예정이므로, 내장된 StringSerializer를 지정한다.
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // Properties 오브젝트를 전달하여 새 프로듀서를 생성한다.
+        // 참고) 아래의 코드에 의해서 producer.close() 메소드를 호출하고, 이를 통해 커넥션 리소스를 정리해준다.
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            String topic = "hyo-topic2";
+            String oddKey = "1";
+            String evenKey = "2";
+
+            for (int i = 1; i < 11; i++) {
+                String value;
+
+                if (i % 2 == 1) {
+                    value = String.format("%d - Apache Kafka is a distributed streaming platform - key=" + oddKey, i);
+                    producer.send(new ProducerRecord<>(topic, oddKey, value));
+                } else {
+                    value = String.format("%d - Apache Kafka is a distributed streaming platform - key=" + evenKey, i);
+                    producer.send(new ProducerRecord<>(topic, evenKey, value));
+                }
+
+                logger.info("send value: {}", value);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
