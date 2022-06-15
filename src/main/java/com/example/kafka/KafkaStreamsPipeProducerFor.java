b3bbde724b99a1695da32c799b0a197f6fc452ef
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-15 16:18:26 +0900

    feat: For 루프를 활용한 Pipe Producer 구현

---
+++ src/main/java/com/example/kafka/KafkaStreamsPipeProducerFor.java
@@ -0,0 +1,27 @@
+package com.example.kafka;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class KafkaStreamsPipeProducerFor {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ProducerConfigValue.BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.ACKS_CONFIG, ProducerConfigValue.ACK_1);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            for (int i = 0; i < 100; i++) {
+                producer.send(new ProducerRecord<>(KafkaTopic.STREAMS_PLAINTEXT_INPUT, "Kafka Streams!"));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
