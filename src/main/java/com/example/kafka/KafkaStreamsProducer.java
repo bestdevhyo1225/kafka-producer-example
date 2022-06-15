ba1469c5c6b265f456c7fcbb74baee484e41a1a7
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-15 15:20:20 +0900

    feat: streams-plaintext-input 토픽으로 메세지를 발행하는 Producer 구현

---
+++ src/main/java/com/example/kafka/KafkaStreamsProducer.java
@@ -0,0 +1,34 @@
+package com.example.kafka;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaStreamsProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaStreamsProducer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ProducerConfigValue.BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.ACKS_CONFIG, ProducerConfigValue.ACK_1);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            RecordMetadata metadata = producer
+                .send(new ProducerRecord<>(KafkaTopic.STREAMS_PLAINTEXT_INPUT, "Kafka Streams!"))
+                .get();
+
+            logger.info("Partition: {}, Offset: {}", metadata.partition(), metadata.offset());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/kafka/KafkaTopic.java
+++ src/main/java/com/example/kafka/KafkaTopic.java
@@ -2,4 +2,5 @@ package com.example.kafka;
 
 public abstract class KafkaTopic {
     public static final String HYO_TOPIC = "hyo-topic";
+    public static final String STREAMS_PLAINTEXT_INPUT = "streams-plaintext-input";
 }
