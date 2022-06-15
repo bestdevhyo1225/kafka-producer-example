41ab8de796738d065187eb98ee9ff5c91d47a617
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-15 15:26:16 +0900

    style: Streams Producer 클래스 이름 변경

--- src/main/java/com/example/kafka/KafkaStreamsPipeProducer.java
+++ src/main/java/com/example/kafka/KafkaStreamsPipeProducer.java
@@ -10,9 +10,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KafkaStreamsProducer {
+public class KafkaStreamsPipeProducer {
 
-    private static final Logger logger = LoggerFactory.getLogger(KafkaStreamsProducer.class);
+    private static final Logger logger = LoggerFactory.getLogger(KafkaStreamsPipeProducer.class);
 
     public static void main(String[] args) {
         Properties props = new Properties();
