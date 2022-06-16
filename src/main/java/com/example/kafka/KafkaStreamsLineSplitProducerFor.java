eae4d130fc130bc781b7f1efa9ae62f230ce9c07
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-16 22:04:35 +0900

    feat: 카프카 스트림즈 - 행 분리 프로듀서 예제 추가

---
+++ src/main/java/com/example/kafka/KafkaStreamsLineSplitProducerFor.java
@@ -0,0 +1,30 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaStreamsLineSplitProducerFor {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ProducerConfigValue.BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.ACKS_CONFIG, ProducerConfigValue.ACK_1);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            String[] sendMessages = {"Apple is Good", "Kafka is Good", "Happy Kafka and Apple"};
+
+            for (int i = 0; i < 3; i++) {
+                producer.send(new ProducerRecord<>(KafkaTopic.STREAMS_PLAINTEXT_INPUT, sendMessages[i]));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
