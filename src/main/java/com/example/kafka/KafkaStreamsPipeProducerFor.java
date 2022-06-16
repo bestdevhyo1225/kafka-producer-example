276e6ea36f5cb7af9d2ec0cfd15ec863a8fcb9ed
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-16 21:50:17 +0900

    feat: 메시지 50개로 변경

--- src/main/java/com/example/kafka/KafkaStreamsPipeProducerFor.java
+++ src/main/java/com/example/kafka/KafkaStreamsPipeProducerFor.java
@@ -17,7 +17,7 @@ public class KafkaStreamsPipeProducerFor {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 50; i++) {
                 producer.send(new ProducerRecord<>(KafkaTopic.STREAMS_PLAINTEXT_INPUT, "Kafka is Good"));
             }
         } catch (Exception exception) {
