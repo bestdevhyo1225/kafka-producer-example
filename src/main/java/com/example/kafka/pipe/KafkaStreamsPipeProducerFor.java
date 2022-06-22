943256793ee089106d47cf13d9344dd205febf8d
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-22 22:05:36 +0900

    chore: retries 값은 1로 설정

--- src/main/java/com/example/kafka/pipe/KafkaStreamsPipeProducerFor.java
+++ src/main/java/com/example/kafka/pipe/KafkaStreamsPipeProducerFor.java
@@ -18,6 +18,7 @@ public class KafkaStreamsPipeProducerFor {
         props.put(ProducerConfig.ACKS_CONFIG, ProducerConfigValue.ACK_1);
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, ProducerConfigValue.BUFFER_MEMORY);
         props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, ProducerConfigValue.COMPRESSION_TYPE_LZ4);
+        props.put(ProducerConfig.RETRIES_CONFIG, ProducerConfigValue.RETRIES);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, ProducerConfigValue.BATCH_SIZE);
         props.put(ProducerConfig.LINGER_MS_CONFIG, ProducerConfigValue.LINGER_MS);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
